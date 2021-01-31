cfeaf95cf531718d1331a1dfefa245b94a1e2e12
LuaiGhazi <luaighazi@gmail.com>
2021-01-30 17:28:01 -0800

    Added reset capabilities

--- montyHall.js
+++ montyHall.js
@@ -1,6 +1,8 @@
 const door1 = document.querySelector('#doorImg1')
 const door2 = document.querySelector('#doorImg2')
 const door3 = document.querySelector('#doorImg3')
+const resetButton = document.querySelector('#reset')
+
 
 let clickCount = 0
 let stayWinCount = 0
@@ -19,6 +21,8 @@ let goatDoorArray = ['door1', 'door2', 'door3']
 door1.addEventListener('click', firstDoor)
 door2.addEventListener('click', secondDoor)
 door3.addEventListener('click', thirdDoor)
+resetButton.addEventListener('click', reset)
+
 
 function changePicture() {
     eval(winningDoor).src = "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/car.png"
@@ -142,46 +146,13 @@ function thirdDoor() {
     }
 }
 
+resetButton.addEventListener('click', reset)
 
-
-// function secondDoor() {
-//     if (clickCount === 0 && door2.src == "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door2.png") {
-//         door2.src = "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/goat.jpg";
-//         console.log(clickCount)
-//         doorArray.splice(doorArray.indexOf('door2'), 1)
-//         console.log(doorArray)
-//         let randomElement = [Math.floor(Math.random() * doorArray.length)];
-//         console.log(doorArray[randomElement])
-//         winningDoor = doorArray[randomElement]
-//         clickCount += 1
-//     } else if (door2.src === "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door2.png" && clickCount === 1 && winningDoor == 'door2') {
-//         alert('You won!')
-//     } else if (door2.src === "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door2.png" && clickCount === 1 && winningDoor !== 'door2') {
-//         alert('You lost!')
-//     }
-// }
-
-
-// function thirdDoor() {
-//     if (clickCount === 0 && door3.src == "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door3.png") {
-//         door3.src = "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/goat.jpg";
-//         console.log(clickCount)
-//         doorArray.splice(doorArray.indexOf('door3'), 1)
-//         console.log(doorArray)
-//         let randomElement = [Math.floor(Math.random() * doorArray.length)];
-//         console.log(doorArray[randomElement])
-//         winningDoor = doorArray[randomElement]
-//         clickCount += 1
-//     } else if (door2.src === "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door2.png" && clickCount === 1 && winningDoor == 'door2') {
-//         alert('You won!')
-//     } else if (door2.src === "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door2.png" && clickCount === 1 && winningDoor !== 'door2') {
-//         alert('You lost!')
-//     }
-// }
-
-
-
-
-
-
-// car image - file:///Users/LuaiGhazi/Desktop/montyHall/imgs/car.png 
+function reset() {
+    clickCount = 0;
+    door1.src = "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door1.png"
+    door2.src = "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door2.png"
+    door3.src = "file:///Users/LuaiGhazi/Desktop/montyHall/imgs/Door3.png"
+    carDoorArray = ['door1', 'door2', 'door3']
+    goatDoorArray = ['door1', 'door2', 'door3']
+}
